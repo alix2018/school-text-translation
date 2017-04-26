eeac1e1a40340de7de2388aaa855709b287bde51
Thibaut Buchert <thibaut.buchert@gmail.com>
2017-04-26 15:44:35 +0200

    Test OCRTesseract

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
-#include <opencv2/core/core.hpp>
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-
-#include "opencv2/objdetect/objdetect.hpp"
-
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <algorithm>
+#include <cstdlib>
+#include <opencv2/core/base.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/text.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <cv.hpp>
 
-using namespace cv;
-using namespace std;
+int main(int argc, char **argv) {
+    cv::Mat img;
+    std::string voca = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.:(),@-";
+    
+    img = cv::imread(argv[1]);
 
-Mat src;
+    auto ocr = cv::text::OCRTesseract::create(nullptr, "eng", voca.c_str());
 
-int main( int argc, char** argv )
-{
-    /// Load source image
-    src = imread( argv[1], 1 );
+    std::vector<cv::Rect> rectangles;
+    std::string out;
+    ocr->run(img, out, &rectangles);
 
-    /// Create Window with source image
-    char* source_window = "Source";
-    namedWindow( source_window, CV_WINDOW_AUTOSIZE );
-    imshow( source_window, src );
+    for (auto r : rectangles)
+        cv::rectangle(img, r.tl(), r.br(), cv::Scalar(255, 0, 0));
 
-    waitKey(0);
-    return(0);
+    std::cout << out << std::endl;
+
+    cv::imshow("res", img);
+    cv::waitKey();
+
+    return 0;
 }
